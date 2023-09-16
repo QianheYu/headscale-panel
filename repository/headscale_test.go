5155b4c295ed191db3389b85ec054caedcddb613
QianheYu <112762030+QianheYu@users.noreply.github.com>
2023-09-16 15:28:17 +0000

    Remove redundant types of compound literals

--- repository/headscale_test.go
+++ repository/headscale_test.go
@@ -12,57 +12,57 @@ import (
 func TestHeascaleCheckSync(t *testing.T) {
 	testOldData := make([]*pb.User, 0)
 	//testOldData := []*pb.User{
-	//	&pb.User{
+	//	{
 	//		Id:        "1",
 	//		Name:      "1",
 	//		CreatedAt: timestamppb.New(time.Date(2023, 1, 10, 13, 29, 45, 0, time.UTC)),
 	//	},
-	//	&pb.User{
+	//	{
 	//		Id:        "2",
 	//		Name:      "2",
 	//		CreatedAt: timestamppb.New(time.Date(2023, 2, 10, 13, 29, 45, 0, time.UTC)),
 	//	},
-	//	&pb.User{
+	//	{
 	//		Id:        "3",
 	//		Name:      "3",
 	//		CreatedAt: timestamppb.New(time.Date(2023, 2, 15, 13, 29, 45, 0, time.UTC)),
 	//	},
-	//	&pb.User{
+	//	{
 	//		Id:        "4",
 	//		Name:      "4",
 	//		CreatedAt: timestamppb.New(time.Date(2023, 2, 10, 13, 29, 45, 0, time.UTC)),
 	//	},
-	//	&pb.User{
+	//	{
 	//		Id:        "6",
 	//		Name:      "6",
 	//		CreatedAt: timestamppb.New(time.Date(2023, 2, 10, 13, 29, 45, 0, time.UTC)),
 	//	},
-	//	&pb.User{
+	//	{
 	//		Id:        "7",
 	//		Name:      "7",
 	//		CreatedAt: timestamppb.New(time.Date(2023, 2, 10, 13, 29, 45, 0, time.UTC)),
 	//	},
-	//	&pb.User{
+	//	{
 	//		Id:        "8",
 	//		Name:      "8",
 	//		CreatedAt: timestamppb.New(time.Date(2023, 2, 10, 13, 29, 45, 0, time.UTC)),
 	//	},
-	//	&pb.User{
+	//	{
 	//		Id:        "9",
 	//		Name:      "9",
 	//		CreatedAt: timestamppb.New(time.Date(2023, 2, 10, 13, 29, 45, 0, time.UTC)),
 	//	},
-	//	&pb.User{
+	//	{
 	//		Id:        "10",
 	//		Name:      "10",
 	//		CreatedAt: timestamppb.New(time.Date(2023, 2, 10, 13, 29, 45, 0, time.UTC)),
 	//	},
-	//	&pb.User{
+	//	{
 	//		Id:        "11",
 	//		Name:      "11",
 	//		CreatedAt: timestamppb.New(time.Date(2023, 2, 10, 13, 29, 45, 0, time.UTC)),
 	//	},
-	//	&pb.User{
+	//	{
 	//		Id:        "12",
 	//		Name:      "12",
 	//		CreatedAt: timestamppb.New(time.Date(2023, 3, 10, 13, 29, 45, 0, time.UTC)),
@@ -70,62 +70,62 @@ func TestHeascaleCheckSync(t *testing.T) {
 	//}
 
 	testNewData := []*pb.User{
-		&pb.User{
+		{
 			Id:        "1",
 			Name:      "1",
 			CreatedAt: timestamppb.New(time.Date(2023, 1, 10, 13, 29, 45, 0, time.UTC)),
 		},
-		&pb.User{
+		{
 			Id:        "2",
 			Name:      "2",
 			CreatedAt: timestamppb.New(time.Date(2023, 2, 10, 13, 29, 45, 0, time.UTC)),
 		},
-		&pb.User{
+		{
 			Id:        "3",
 			Name:      "3",
 			CreatedAt: timestamppb.New(time.Date(2023, 2, 15, 13, 29, 45, 0, time.UTC)),
 		},
-		&pb.User{
+		{
 			Id:        "7",
 			Name:      "7",
 			CreatedAt: timestamppb.New(time.Date(2023, 2, 10, 13, 29, 45, 0, time.UTC)),
 		},
-		&pb.User{
+		{
 			Id:        "8",
 			Name:      "8",
 			CreatedAt: timestamppb.New(time.Date(2023, 2, 10, 13, 29, 45, 0, time.UTC)),
 		},
-		&pb.User{
+		{
 			Id:        "9",
 			Name:      "9",
 			CreatedAt: timestamppb.New(time.Date(2023, 2, 10, 13, 29, 45, 0, time.UTC)),
 		},
-		&pb.User{
+		{
 			Id:        "10",
 			Name:      "10",
 			CreatedAt: timestamppb.New(time.Date(2023, 2, 10, 13, 29, 45, 0, time.UTC)),
 		},
-		&pb.User{
+		{
 			Id:        "11",
 			Name:      "11",
 			CreatedAt: timestamppb.New(time.Now()),
 		},
-		//&pb.User{
+		//{
 		//	Id:        "12",
 		//	Name:      "12",
 		//	CreatedAt: timestamppb.New(time.Now()),
 		//},
-		//&pb.User{
+		//{
 		//	Id:        "13",
 		//	Name:      "13",
 		//	CreatedAt: timestamppb.New(time.Now()),
 		//},
-		//&pb.User{
+		//{
 		//	Id:        "14",
 		//	Name:      "14",
 		//	CreatedAt: timestamppb.New(time.Now()),
 		//},
-		//&pb.User{
+		//{
 		//	Id:        "15",
 		//	Name:      "15",
 		//	CreatedAt: timestamppb.New(time.Now()),
@@ -143,62 +143,62 @@ func TestHeascaleCheckSync(t *testing.T) {
 
 func TestSearchUser(t *testing.T) {
 	testNewData := []*pb.User{
-		&pb.User{
+		{
 			Id:        "1",
 			Name:      "1",
 			CreatedAt: timestamppb.New(time.Date(2023, 1, 10, 13, 29, 45, 0, time.UTC)),
 		},
-		&pb.User{
+		{
 			Id:        "2",
 			Name:      "2",
 			CreatedAt: timestamppb.New(time.Date(2023, 2, 10, 13, 29, 45, 0, time.UTC)),
 		},
-		&pb.User{
+		{
 			Id:        "3",
 			Name:      "3",
 			CreatedAt: timestamppb.New(time.Date(2023, 2, 15, 13, 29, 45, 0, time.UTC)),
 		},
-		&pb.User{
+		{
 			Id:        "7",
 			Name:      "7",
 			CreatedAt: timestamppb.New(time.Date(2023, 2, 10, 13, 29, 45, 0, time.UTC)),
 		},
-		&pb.User{
+		{
 			Id:        "8",
 			Name:      "8",
 			CreatedAt: timestamppb.New(time.Date(2023, 2, 10, 13, 29, 45, 0, time.UTC)),
 		},
-		&pb.User{
+		{
 			Id:        "9",
 			Name:      "9",
 			CreatedAt: timestamppb.New(time.Date(2023, 2, 10, 13, 29, 45, 0, time.UTC)),
 		},
-		&pb.User{
+		{
 			Id:        "10",
 			Name:      "10",
 			CreatedAt: timestamppb.New(time.Date(2023, 2, 10, 13, 29, 45, 0, time.UTC)),
 		},
-		&pb.User{
+		{
 			Id:        "11",
 			Name:      "11",
 			CreatedAt: timestamppb.New(time.Now()),
 		},
-		&pb.User{
+		{
 			Id:        "12",
 			Name:      "12",
 			CreatedAt: timestamppb.New(time.Now()),
 		},
-		&pb.User{
+		{
 			Id:        "13",
 			Name:      "13",
 			CreatedAt: timestamppb.New(time.Now()),
 		},
-		&pb.User{
+		{
 			Id:        "14",
 			Name:      "14",
 			CreatedAt: timestamppb.New(time.Now()),
 		},
-		&pb.User{
+		{
 			Id:        "15",
 			Name:      "15",
 			CreatedAt: timestamppb.New(time.Now()),
